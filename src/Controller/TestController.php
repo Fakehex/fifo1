9d174b7e49f43359ed77e402e07c64d085c4b35d
Hadji Gizel <gizel.hadji@etu.univ-orleans.fr>
2019-04-03 11:39:46 +0200

    essai via linux

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -5,6 +5,10 @@ use Symfony\Component\HttpFoundation\Response;
 
 class TestController {
   public function index(){
+<<<<<<< 2a5e58c3a31dc961e5a49cdaaaf4751236799d9b
     return new Response('jpp3');
+=======
+    return new Response('helloworld');
+>>>>>>> essai via linux
   }
 }
