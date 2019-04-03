c819d15e8f1b5f545b2b35e7899548989f9fb0f6
Hadji Gizel <gizel.hadji@etu.univ-orleans.fr>
2019-04-03 11:39:46 +0200

    essai via linux

---
+++ src/Controller/EvenementController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Evenement;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+
+class EvenementController extends AbstractController
+{
+    /**
+     * @Route("/evenement", name="evenement")
+     */
+    public function index()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $user1 = $entityManager->getRepository(User::class)->find(1);
+        $user2 = $entityManager->getRepository(User::class)->find(2);
+
+        $event = new Evenement();
+        $event->setTitre("secondTry avec users");
+        $event->setTexte("yeah body light weight");
+        $event->addParticipant($user1);
+        $event->addParticipant($user2);
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($event);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$event->getId().' et les texte '.$event->getTexte());
+    }
+
+    /**
+     * @Route("/evenement/{id}", name="evenemnt_show")
+     */
+    public function show($id)
+    {
+        $event = $this->getDoctrine()
+            ->getRepository(Evenement::class)
+            ->find($id);
+
+        if (!$event) {
+            throw $this->createNotFoundException(
+                "pas d'event pour " . $id
+            );
+        }
+
+        return new Response('Vla le grand event : ' . $event->getTitre());
+    }
+}
---
+++ src/Entity/Evenement.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EvenementRepository")
+ */
+class Evenement
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $texte;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\User", inversedBy="evenements")
+     */
+    private $participants;
+
+    public function __construct()
+    {
+        $this->participants = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getTexte(): ?string
+    {
+        return $this->texte;
+    }
+
+    public function setTexte(string $texte): self
+    {
+        $this->texte = $texte;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|User[]
+     */
+    public function getParticipants(): Collection
+    {
+        return $this->participants;
+    }
+
+    public function addParticipant(User $participant): self
+    {
+        if (!$this->participants->contains($participant)) {
+            $this->participants[] = $participant;
+        }
+
+        return $this;
+    }
+
+    public function removeParticipant(User $participant): self
+    {
+        if ($this->participants->contains($participant)) {
+            $this->participants->removeElement($participant);
+        }
+
+        return $this;
+    }
+}
