2257345617d0f2155f38420adf1626869fccff41
TofuPlatinum <BestGitEU45>
2019-04-08 12:03:40 +0200

    connection Utilisateur avec roles à la clé, fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,11 +5,14 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
 
 
 class UserFixtures extends Fixture
 {
     private $passwordEncoder;
+    private $roles;
 
     public function __construct(UserPasswordEncoderInterface $passwordEncoder){
         $this->passwordEncoder;
@@ -18,10 +21,10 @@ class UserFixtures extends Fixture
     {
         $user = new User();
 
-        $user->setUsername('Oui');
+        $user->setUsername('oui');
         $user->setEmail('oui@oui.oui');
-        $user->setPassword($this->passwordEncoder->encodePassword($user,'the_new_password'));
-
+        $user->setPassword('test');
+        //$user->setPassword($this->passwordEncoder->encodePassword($user,'the_new_password'));
         $manager->persist($user);
 
         $manager->flush();
