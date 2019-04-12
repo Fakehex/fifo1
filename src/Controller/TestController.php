85263aceaf8fa9707bd281f2faab47767149c853
Hadji Gizel <gizel.hadji@etu.univ-orleans.fr>
2019-04-12 11:22:59 +0200

    frontEnd archives etc

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -5,10 +5,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/", name="accueil")
- */
+
 class TestController extends AbstractController {
+  /**
+   * @Route("/", name="accueil")
+   */
   public function index(){
     return $this->render('accueil/index.html.twig');
   }
