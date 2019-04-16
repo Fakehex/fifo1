085b9a6cf85337b1245ae3e0c6312dee6f87ffa1
Hadji Gizel <gizel.hadji@etu.univ-orleans.fr>
2019-04-16 16:55:17 +0200

    test sur la session

--- src/Controller/AdminDefaultController.php
+++ src/Controller/AdminDefaultController.php
@@ -4,13 +4,30 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
-
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+/**
+ * @Route("/admin")
+ */
 class AdminDefaultController extends AbstractController {
   /**
-   * @Route("/admin", name="admin")
+   * @Route("/", name="admin")
    */
   public function index(){
     return $this->render('adminDefault/index.html.twig');
+  }
+  /**
+   * @Route("/s1", name="s1")
+   */
+  public function s1(SessionInterface $session){
+    $s1 = "ca marche";
+    $session->set('s1',$s1);
+    return $this->render('test.html.twig', ['s1' => "preparation"]);
+  }
+  /**
+   * @Route("/s2", name="s2")
+   */
+  public function s2(SessionInterface $session){
+    $s1 = $session->get('s1');
+    return $this->render('test.html.twig',['s1' => $s1]);
   }
 }
