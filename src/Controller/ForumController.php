d3903803645799e59bd13d1899447659d07161fe
Hadji Gizel <gizel.hadji@etu.univ-orleans.fr>
2019-04-18 14:42:12 +0200

    user / topic relation

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -66,4 +66,27 @@ class ForumController extends AbstractController {
     ]);
   }
 
+  /**
+   * @Route("/{slugCategorie}/nouveau", name="nouveau_topic", methods={"GET","POST"})
+   */
+  public function new(Request $request): Response
+  {
+      $topic = new Topic();
+      $form = $this->createForm(TopicType::class, $topic);
+      $form->handleRequest($request);
+
+      if ($form->isSubmitted() && $form->isValid()) {
+          $entityManager = $this->getDoctrine()->getManager();
+          $entityManager->persist($topic);
+          $entityManager->flush();
+
+          return $this->redirectToRoute('topic_index');
+      }
+
+      return $this->render('topic/new.html.twig', [
+          'topic' => $topic,
+          'form' => $form->createView(),
+      ]);
+  }
+
 }
